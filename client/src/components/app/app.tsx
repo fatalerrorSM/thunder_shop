73073aada93845590038d2bba074f09a373c9728
Fatal Error <fatalerror.sm@gmail.com>
2019-05-05 23:58:35 +0300

    create a entry point for application

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,26 +1,13 @@
 import * as React from 'react';
-
+import Header from "../header";
+import Content from "../content";
 
 export default class App extends React.Component{
-    state = {
-        id : 0
-    }
-    obj = fetch('/admin').then(res => {
-        res.json().then(data => {
-            this.setState({id : data.id});
-        })
-    });
-    increment = () =>{
-        this.setState({
-            id : (this.state.id + 1)
-        })
-    }
     render(){
-        const {id} = this.state
         return(
             <div>
-                <p>Hello {id}</p>
-                <button onClick={()=>this.increment()}></button>
+                <Header></Header>
+                <Content></Content>
             </div>
         )
     }
