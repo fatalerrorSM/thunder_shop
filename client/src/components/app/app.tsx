36bd67e176b9da1c3e9ca0235cdbbfaf76458375
Fatal Error <fatalerror.sm@gmail.com>
2019-05-07 21:48:36 +0300

    add footer to core app component

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Header from "../header";
 import Content from "../content";
-
+import Footer from "../footer"
 export default class App extends React.Component{
     render(){
         return(
             <div>
                 <Header></Header>
                 <Content></Content>
+                <Footer></Footer>
             </div>
         )
     }
