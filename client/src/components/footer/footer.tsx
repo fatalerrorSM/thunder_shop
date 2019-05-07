6d58b0d91093bc9d1a4059e9422d1fa822af948c
Fatal Error <fatalerror.sm@gmail.com>
2019-05-07 21:48:05 +0300

    new component for client side - footer

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./footer.css";
+
+export default class Footer extends React.Component{
+    render(){
+        return(
+            <div className="footer">
+                <a uk-icon="icon: reddit" className="uk-margin-right"></a>
+                <a uk-icon="icon: github" className="uk-margin-right"></a>
+                <a uk-icon="icon: facebook"></a>
+            </div>
+        )
+    }
+}
