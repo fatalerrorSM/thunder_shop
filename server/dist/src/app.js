ac7f8664e84ed3c29ca226045d8177cefeb1da69
Fatal Error <fatalerror.sm@gmail.com>
2019-05-07 21:51:18 +0300

    add mongoDB(settings,dependencies) and change controllers

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -11,24 +11,56 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+const express_session_1 = __importDefault(require("express-session"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_validator_1 = __importDefault(require("express-validator"));
 const path_1 = __importDefault(require("path"));
+const connect_mongo_1 = __importDefault(require("connect-mongo"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const secrets_1 = require("./utils/secrets");
 //Controllers (route handlers)
 const homeController = __importStar(require("./controllers/home"));
-const adminController = __importStar(require("./controllers/admin"));
-//Load env variables from .env files
-dotenv_1.default.config({ path: ".env" });
+const categoriesController = __importStar(require("./controllers/category"));
+const MongoStore = connect_mongo_1.default(express_session_1.default);
 //Create Express server
 const app = express_1.default();
+// Connect to MongoDB
+const mongoUrl = secrets_1.MONGODB_URI;
+const session_secret = secrets_1.SESSION_SECRET;
+mongoose_1.default
+    .connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+})
+    .catch((err) => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
+});
 //Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path_1.default.join(__dirname, "../views"));
 app.set("view engine", "pug");
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.use(express_validator_1.default());
+app.use(express_session_1.default({
+    resave: true,
+    saveUninitialized: true,
+    secret: session_secret,
+    store: new MongoStore({
+        url: mongoUrl,
+        autoReconnect: true
+    })
+}));
 /*
  * Primary app routes
  */
-// User Zone
+// Default Zone (entry point for test app)
 app.get("/", homeController.index);
-// Admins Zone
-app.get("/admin", adminController.getAdminPage);
+// Categories Zone
+app.get("/categories", categoriesController.getAllcategories);
+app.get('/categories/:id', categoriesController.getCategory);
+app.post("/categories", categoriesController.addCategory);
+app.put("/categories/:id", categoriesController.updateCategory);
+app.delete("/categories/:id", categoriesController.deleteCategory);
 exports.default = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,32 +1,67 @@
 import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import expressValidator from "express-validator";
 import path from "path";
+import mongo from "connect-mongo";
+import mongoose from "mongoose";
+import {MONGODB_URI,SESSION_SECRET} from "./utils/secrets";
 //Controllers (route handlers)
 import * as homeController from "./controllers/home";
-import * as adminController from "./controllers/admin";
+import * as categoriesController from "./controllers/category";
 
-
-//Load env variables from .env files
-dotenv.config({ path: ".env" });
+const MongoStore = mongo(session);
 
 //Create Express server
 const app = express();
+
+// Connect to MongoDB
+const mongoUrl : string | any = MONGODB_URI;
+
+const session_secret : string | any = SESSION_SECRET;
+
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true,useCreateIndex: true, useFindAndModify: false  })
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err: Error) => {
+    console.log(
+      "MongoDB connection error. Please make sure MongoDB is running. " + err
+    );
+    // process.exit();
+  });
 
 //Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: session_secret,
+    store: new MongoStore({
+      url: mongoUrl,
+      autoReconnect: true
+    })
+}));
 
 /*
  * Primary app routes
  */
 
-// User Zone
-
+// Default Zone (entry point for test app)
 app.get("/", homeController.index);
 
-// Admins Zone
-
-app.get("/admin",adminController.getAdminPage)
+// Categories Zone
+app.get("/categories", categoriesController.getAllcategories);
+app.get('/categories/:id',categoriesController.getCategory);
+app.post("/categories", categoriesController.addCategory);
+app.put("/categories/:id",categoriesController.updateCategory);
+app.delete("/categories/:id", categoriesController.deleteCategory);
 
 export default app;
