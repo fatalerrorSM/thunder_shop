07cdf5da833e99116d92a3b777e100261a619f98
Fatal Error <fatalerror.sm@gmail.com>
2019-05-07 21:50:04 +0300

    refactoring

--- server/dist/src/controllers/admin.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAdminPage = (req, res) => {
-    res.json({ "id": 1 });
-};
--- server/src/controllers/admin.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export let getAdminPage = (req: Request, res: Response) => {
-  res.json({"id" : 1});
-};
