f9154852acb86eb2724a5385597fd386f042ecbf
Fatal Error <fatalerror.sm@gmail.com>
2019-05-13 23:17:55 +0300

    created a sign in page

--- server/dist/controllers/home.js
+++ server/dist/controllers/home.js
@@ -1,10 +1,44 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = (req, res) => {
-    res.render("home", {
-        title: "home"
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.postSignIn = (req, res) => {
-    console.log("enter");
+Object.defineProperty(exports, "__esModule", { value: true });
+const passport_1 = __importDefault(require("passport"));
+exports.index = (req, res) => {
+    if (req.user) {
+        res.redirect("/admin");
+    }
+    else {
+        res.render("home", {
+            title: "home"
+        });
+    }
+};
+exports.postSignIn = (req, res, next) => {
+    console.log(req.body.user_name, req.body.password);
+    req.assert("user_name", " is not valid").len({ min: 1 });
+    req.assert("password", "Password cannot be blank").notEmpty();
+    const errors = req.validationErrors();
+    if (errors) {
+        console.log("errors", errors);
+        res.render("home", {
+            st: false,
+            msg: "User name or password incorrect"
+        });
+        return res.redirect("/");
+    }
+    passport_1.default.authenticate("local", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.render("home", { st: false, msg: "User name or password incorrect" });
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect("/admin");
+        });
+    })(req, res, next);
 };
--- server/src/controllers/home.ts
+++ server/src/controllers/home.ts
@@ -1,11 +1,43 @@
-import { Request, Response } from "express";
+import { Request, Response , NextFunction} from "express";
+import passport from "passport";
+import { default as Admin, AdminModel } from "../models/Admin";
+import { IVerifyOptions } from "passport-local";
+
 
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "home"
-  });
+  if(req.user){
+    res.redirect("/admin");
+  }else{
+    res.render("home", {
+      title: "home"
+    });
+  }
 };
 
-export let postSignIn = (req: Request, res: Response) => {
-  console.log("enter");
+export let postSignIn = (req: Request, res: Response,next : NextFunction) => {
+  console.log(req.body.user_name, req.body.password);
+  req.assert("user_name", " is not valid").len({min: 1});
+  req.assert("password", "Password cannot be blank").notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    console.log("errors", errors);
+    res.render("home",{
+      st : false,
+      msg : "User name or password incorrect"
+    })
+    return res.redirect("/");
+  }
+
+  passport.authenticate("local", (err: Error, user: AdminModel, info: IVerifyOptions) => {
+    if (err) { return next(err); }
+    if (!user) {
+      return res.render("home",{st:false,msg:"User name or password incorrect"});
+    }
+    req.logIn(user, (err) => {
+      if (err) { return next(err); }
+      res.redirect("/admin");
+    });
+  })(req, res, next);
 };
