afecb2c9e14a4edf5d7051e04af19ae55a937d62
Fatal Error <fatalerror.sm@gmail.com>
2019-05-15 23:52:39 +0300

    adding add/update/delete function to categories page

--- server/dist/controllers/admin.js
+++ server/dist/controllers/admin.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Admin_1 = __importDefault(require("../models/Admin"));
 const Category_1 = require("../models/Category");
 const pug_1 = require("pug");
+const got_1 = __importDefault(require("got"));
 exports.getAdmin = (req, res) => {
     res.render("admin", {
         title: "admin"
@@ -41,14 +42,104 @@ exports.getCategories = (req, res) => {
 };
 exports.postCategories = (req, res) => {
     req.assert("radio").notEmpty();
+    req.assert("name").len({ min: 1, max: 15 });
+    req.assert("image").notEmpty();
+    req.assert("id");
     const errors = req.validationErrors();
-    console.log(errors);
     if (errors) {
-        pug_1.render("categories", {
+        return res.render("categories", {
             st: false,
             title: "categories",
-            error: ""
+            error: "Something goes wrong try again"
         });
+    }
+    switch (req.body.radio) {
+        case "add-radio": {
+            let options = {
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: req.body.name,
+                    image: req.body.image
+                })
+            };
+            got_1.default
+                .post(`${process.env.LOCAL_URL}/categories`, options)
+                .then(response => {
+                if (response.statusCode === 201) {
+                    return res.redirect("categories");
+                }
+                else {
+                    return res.render("categories", {
+                        st: false,
+                        title: "categories",
+                        error: response.statusMessage
+                    });
+                }
+            })
+                .catch((err) => {
+                console.error(err.message);
+            });
+            break;
+        }
+        case "update-radio": {
+            console.log(req.body.id);
+            if (req.body.id) {
+                const options = {
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: req.body.name,
+                        image: req.body.image
+                    })
+                };
+                got_1.default.put(`${process.env.LOCAL_URL}/categories/${req.body.id}`, options).then(response => {
+                    if (response.statusCode === 200) {
+                        return res.redirect("categories");
+                    }
+                    else {
+                        return pug_1.render("categories", {
+                            st: false,
+                            title: "categories",
+                            error: response.statusMessage
+                        });
+                    }
+                }).catch((err) => {
+                    console.error(err.message);
+                });
+            }
+            else {
+                res.render("categories", {
+                    st: false,
+                    title: "categories",
+                    error: "ID is not selected"
+                });
+            }
+            break;
+        }
+        case "delete-radio": {
+            if (req.body.id) {
+                got_1.default.delete(`${process.env.LOCAL_URL}/categories/${req.body.id}`).then(response => {
+                    if (response.statusCode === 200) {
+                        return res.redirect("categories");
+                    }
+                    else {
+                        res.render("categories", {
+                            st: false,
+                            title: "categories",
+                            error: "Something goes wrong try again"
+                        });
+                    }
+                }).catch((err) => {
+                    console.error(err.message);
+                });
+            }
+            break;
+        }
+        default:
+            break;
     }
 };
 exports.getItems = (req, res) => {
@@ -129,4 +220,103 @@ exports.postAdmin = (req, res, next) => {
 exports.logout = (req, res) => {
     req.logout();
     res.redirect("/");
+};
+exports.addCategory = (req, res, next) => {
+    req.assert("radio").notEmpty();
+    req.assert("name").len({ min: 1, max: 15 });
+    req.assert("image").notEmpty();
+    req.assert("name").len({ min: 1, max: 15 });
+    req.assert("image").notEmpty();
+    const errors = req.validationErrors();
+    if (errors) {
+        return res.render("categories", {
+            st: false,
+            title: "categories",
+            error: ""
+        });
+    }
+    const category = new Category_1.Category({
+        name: req.body.name,
+        image: req.body.image
+    });
+    Category_1.Category.findOne({ name: req.body.name }, (err, existngCategory) => {
+        if (err) {
+            return next(err);
+        }
+        if (existngCategory) {
+            res.status(500).send("Category is already exist");
+        }
+        category.save(err => {
+            if (err) {
+                return next(err);
+            }
+            res.status(201).json(category);
+        });
+    });
+};
+exports.deleteCategory = (req, res) => {
+    let category = Category_1.Category.findByIdAndDelete(req.params.id)
+        .then((result) => {
+        if (!result) {
+            return res
+                .status(500)
+                .send(`Can't delete document with id -> ${req.params.id}`);
+        }
+        else {
+            return res
+                .status(200)
+                .send(`Document with id -> ${req.params.id} successfully deleted`);
+        }
+    })
+        .catch((err) => {
+        console.error(err.message);
+    });
+};
+exports.updateCategory = (req, res) => {
+    if (!req.body.name && !req.body.image) {
+        return res.status(202).send("Object with options for update is empty");
+    }
+    else if (req.body.name && !req.body.image) {
+        let category = Category_1.Category.findOneAndUpdate(req.params.id, {
+            name: req.body.name
+        }).then((result) => {
+            if (!result) {
+                return res
+                    .status(500)
+                    .send(`Cannot update category with id ${req.params.id} , and body parametrs ${req.body.name}`);
+            }
+            else {
+                return res.status(200).json(category);
+            }
+        });
+    }
+    else if (!req.body.name && req.body.image) {
+        let category = Category_1.Category.findOneAndUpdate(req.params.id, {
+            image: req.body.image
+        }).then((result) => {
+            if (!result) {
+                return res
+                    .status(500)
+                    .send(`Cannot update category with id ${req.params.id} , and body parametrs ${req.body.image}`);
+            }
+            else {
+                return res.status(200).json(category);
+            }
+        });
+    }
+    else if (req.body.name && req.body.image) {
+        let category = Category_1.Category.findOneAndUpdate(req.params.id, {
+            name: req.body.name,
+            image: req.body.image
+        }).then((result) => {
+            if (!result) {
+                return res
+                    .status(500)
+                    .send(`Cannot update category with id ${req.params.id} , and body parametrs ${req.body.name} ${req.body.image}`);
+            }
+            else {
+                return res.status(200).json(category);
+            }
+        });
+    }
 };
--- server/src/controllers/admin.ts
+++ server/src/controllers/admin.ts
@@ -1,9 +1,8 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 import { default as Admin } from "../models/Admin";
 import { Category } from "../models/Category";
 import { render } from "pug";
 import got from "got";
-
 
 export let getAdmin = (req: Request, res: Response) => {
   res.render("admin", {
@@ -25,12 +24,11 @@ export let getCategories = (req: Request, res: Response) => {
     } else {
       let formatResult: any = [];
       result.forEach(category => {
-
         let obj = {
-          id : category._id,
-          name : category.name,
-          image : category.image
-        }
+          id: category._id,
+          name: category.name,
+          image: category.image
+        };
 
         formatResult.push(obj);
       });
@@ -44,24 +42,107 @@ export let getCategories = (req: Request, res: Response) => {
   });
 };
 
-export let postCategories = (req : Request, res : Response) => {
+export let postCategories = (req: Request, res: Response) => {
   req.assert("radio").notEmpty();
   req.assert("name").len({ min: 1, max: 15 });
   req.assert("image").notEmpty();
-
+  req.assert("id");
 
   const errors = req.validationErrors();
 
-  console.log(errors);
-
   if (errors) {
-    render("categories",{
-      st:false,
+    return res.render("categories", {
+      st: false,
       title: "categories",
-      error : ""
-    })
+      error: "Something goes wrong try again"
+    });
   }
-}
+
+  switch (req.body.radio) {
+    case "add-radio": {
+      let options = {
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          name: req.body.name,
+          image: req.body.image
+        })
+      };
+      got
+        .post(`${process.env.LOCAL_URL}/categories`, options)
+        .then(response => {
+          if (response.statusCode === 201) {
+            return res.redirect("categories");
+          } else {
+            return res.render("categories", {
+              st: false,
+              title: "categories",
+              error: response.statusMessage
+            });
+          }
+        })
+        .catch((err: Error) => {
+          console.error(err.message);
+        });
+      break;
+    }
+    case "update-radio":{
+      console.log(req.body.id);
+      if(req.body.id){
+        const options = {
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({
+            name: req.body.name,
+            image: req.body.image
+          })
+        };
+        got.put(`${process.env.LOCAL_URL}/categories/${req.body.id}`,options).then(response => {
+          if(response.statusCode === 200){
+            return res.redirect("categories");
+          }else{
+            return render("categories",{
+              st:false,
+              title : "categories",
+              error : response.statusMessage
+            })
+          }
+        }).catch((err : Error) => {
+          console.error(err.message);
+        })
+      }else{
+        res.render("categories",{
+          st:false,
+          title : "categories",
+          error: "ID is not selected"
+        })
+      }
+      break;
+    }
+    case "delete-radio":{
+      if(req.body.id){
+        got.delete(`${process.env.LOCAL_URL}/categories/${req.body.id}`).then(response =>{
+          if(response.statusCode === 200){
+            return res.redirect("categories");
+          }else{
+            res.render("categories",{
+              st:false,
+              title : "categories",
+              error: "Something goes wrong try again"
+            })
+          }
+        }).catch((err : Error) =>{
+          console.error(err.message);
+        })
+      }
+      break;}
+
+    default:
+      break;
+  }
+};
 
 export let getItems = (req: Request, res: Response) => {
   res.render("items", {
@@ -149,4 +230,116 @@ export let postAdmin = (req: Request, res: Response, next: NextFunction) => {
 export let logout = (req: Request, res: Response) => {
   req.logout();
   res.redirect("/");
+};
+
+export let addCategory = (req: Request, res: Response, next: NextFunction) => {
+  req.assert("radio").notEmpty();
+  req.assert("name").len({ min: 1, max: 15 });
+  req.assert("image").notEmpty();
+
+  req.assert("name").len({ min: 1, max: 15 });
+  req.assert("image").notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    return res.render("categories", {
+      st: false,
+      title: "categories",
+      error: ""
+    });
+  }
+
+  const category = new Category({
+    name: req.body.name,
+    image: req.body.image
+  });
+
+  Category.findOne({ name: req.body.name }, (err, existngCategory) => {
+    if (err) {
+      return next(err);
+    }
+    if (existngCategory) {
+      res.status(500).send("Category is already exist");
+    }
+    category.save(err => {
+      if (err) {
+        return next(err);
+      }
+      res.status(201).json(category);
+    });
+  });
+};
+
+export let deleteCategory = (req: Request, res: Response) => {
+  let category = Category.findByIdAndDelete(req.params.id)
+    .then((result: any) => {
+      if (!result) {
+        return res
+          .status(500)
+          .send(`Can't delete document with id -> ${req.params.id}`);
+      } else {
+        return res
+          .status(200)
+          .send(`Document with id -> ${req.params.id} successfully deleted`);
+      }
+    })
+    .catch((err: Error) => {
+      console.error(err.message);
+    });
+};
+
+export let updateCategory = (req: Request, res: Response) => {
+  if (!req.body.name && !req.body.image) {
+    return res.status(202).send("Object with options for update is empty");
+  } else if (req.body.name && !req.body.image) {
+    let category: any = Category.findOneAndUpdate(req.params.id, {
+      name: req.body.name
+    }).then((result: any) => {
+      if (!result) {
+        return res
+          .status(500)
+          .send(
+            `Cannot update category with id ${
+              req.params.id
+            } , and body parametrs ${req.body.name}`
+          );
+      } else {
+        return res.status(200).json(category);
+      }
+    });
+  } else if (!req.body.name && req.body.image) {
+    let category: any = Category.findOneAndUpdate(req.params.id, {
+      image: req.body.image
+    }).then((result: any) => {
+      if (!result) {
+        return res
+          .status(500)
+          .send(
+            `Cannot update category with id ${
+              req.params.id
+            } , and body parametrs ${req.body.image}`
+          );
+      } else {
+        return res.status(200).json(category);
+      }
+    });
+  } else if (req.body.name && req.body.image) {
+    let category: any = Category.findOneAndUpdate(req.params.id, {
+      name: req.body.name,
+      image: req.body.image
+    }).then((result: any) => {
+      if (!result) {
+        return res
+          .status(500)
+          .send(
+            `Cannot update category with id ${
+              req.params.id
+            } , and body parametrs ${req.body.name} ${req.body.image}`
+          );
+      } else {
+        return res.status(200).json(category);
+      }
+    });
+  }
 };
