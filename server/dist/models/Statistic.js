f630ba10a6d5bd9ff5853bef50cde8d97e964247
Fatal Error <fatalerror.sm@gmail.com>
2019-05-27 22:24:12 +0300

    Statistics model

--- server/dist/models/Statistic.js
+++ server/dist/models/Statistic.js
@@ -1,1 +1,18 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importStar(require("mongoose"));
+const statisticSchema = new mongoose_1.Schema({
+    DAY: { type: String, required: true },
+    MONTHS: { type: String, required: true, unique: true },
+    YEAR: { type: String, required: true },
+    price: { type: String }
+});
+const Statistic = mongoose_1.default.model("Statistic", statisticSchema);
+exports.default = Statistic;
---
+++ server/src/models/Statistic.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+import IStatistic from "../interfaces/statistic";
+
+export type StatisticModel = mongoose.Document & {
+  DAY: string;
+  MONTHS: string;
+  YEAR: string;
+  price: string;
+};
+
+const statisticSchema: Schema = new Schema({
+  DAY: { type: String, required: true },
+  MONTHS: { type: String, required: true , unique : true},
+  YEAR: { type: String, required: true },
+  price: { type: String }
+});
+
+const Statistic = mongoose.model<StatisticModel>("Statistic", statisticSchema);
+
+export default Statistic;
