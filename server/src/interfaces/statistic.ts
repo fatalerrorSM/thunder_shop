bf224190f6f76b337029ec86bc26b929c85eb80b
Fatal Error <fatalerror.sm@gmail.com>
2019-05-27 22:24:22 +0300

    Statistic interface

--- server/src/interfaces/statistic.ts
+++ server/src/interfaces/statistic.ts
@@ -1,5 +1,8 @@
 import { Document, Schema } from "mongoose";
 
 export default interface IStatistic extends Document {
-
+    DAY: string;
+    MONTHS : string;
+    YEAR: string;
+    price : string;
 }
