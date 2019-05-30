78c4b5a0c1e028c7d1d81035b3530be1153f8659
Fatal Error <fatalerror.sm@gmail.com>
2019-05-30 21:58:47 +0300

    thinking about the changes related to the centralization of data

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -2,13 +2,21 @@ import * as React from 'react';
 import Header from "../header";
 import Content from "../content";
 import Footer from "../footer"
+import TScontroler from "../../service/t_s";
 
 export default class App extends React.Component{
+    tsController = new TScontroler();
+    onClickCategory = (id : any) => {
+        this.tsController.getItems().then((res : any) => {
+            console.log(res);
+        })
+    }
+    
     render(){
         return(
             <div>
                 <Header></Header>
-                <Content></Content>
+                <Content onClickCategory={this.onClickCategory}></Content>
                 <Footer></Footer>                
             </div>
         )
