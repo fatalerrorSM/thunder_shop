063931d9578559b6892cbb8acf53bfccf2565f88
Fatal Error <fatalerror.sm@gmail.com>
2019-05-30 21:56:04 +0300

    adding component for render categories

---
+++ client/src/components/category-item/category-item.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+interface ICategoryItem extends React.Props<any> {
+  name: string;
+  image: string;
+}
+
+export default class CategoryItem extends React.Component<ICategoryItem> {
+  render() {
+    return (
+      <div className="uk-card uk-card-default uk-card-hover">
+        <div className="uk-card-media-top">
+          <img src={this.props.image} alt="" />
+        </div>
+        <div className="uk-card-body">{this.props.name}</div>
+      </div>
+    );
+  }
+}
