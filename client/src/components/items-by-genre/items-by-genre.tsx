933e9d54063c11b592564550d249472d0b318b8e
Fatal Error <fatalerror.sm@gmail.com>
2019-06-12 14:50:34 +0300

    refactoring

--- client/src/components/items-by-genre/items-by-genre.tsx
+++ client/src/components/items-by-genre/items-by-genre.tsx
@@ -1,38 +1,53 @@
 import * as React from "react";
 import Item from "../item";
 
-interface IItems extends React.Props<any>{
-    sItemsByGenre : any;
-    onItemClick? : any;
+interface IItems extends React.Props<any> {
+  sItemsByGenre: any;
+  onItemClick?: any;
+  onClickAddToCart: any;
+  cart: any;
+  onClickDeleteFromCart: any;
 }
 
 interface IItem {
-    id: string;
-    name: string;
-    price : string;
-    discount : string;
-    image: string;
+  id: string;
+  name: string;
+  price: string;
+  discount: string;
+  image: string;
 }
 
-export default class ItemsBG extends React.Component<IItems>{
-    createItems = (res : any) => {
-        return res.map((item: IItem) => {
-          return (
-            <div key={item.id} onClick={() => this.props.onItemClick(item.id)}>
-              <Item name={item.name} price={item.price} discount={item.discount} image={item.image}/>
-            </div>
-          );
-        });
-      };
-    
-    render(){
-      const itemsBG = this.createItems(this.props.sItemsByGenre);
-    
-        return(
-            <div className="uk-text-center uk-child-width-1-3 uk-grid-match"
-            uk-grid="true">
-                {itemsBG}
-            </div>
-        )
-    }
+export default class ItemsBG extends React.Component<IItems> {
+  createItems = (res: any) => {
+    return res.map((item: IItem) => {
+      return (
+        <div key={item.id}>
+          <Item
+            name={item.name}
+            cart={this.props.cart}
+            id={item.id}
+            price={item.price}
+            onClickDeleteFromCart={this.props.onClickDeleteFromCart}
+            onItemClick={this.props.onItemClick}
+            onClickAddToCart={this.props.onClickAddToCart}
+            discount={item.discount}
+            image={item.image}
+          />
+        </div>
+      );
+    });
+  };
+
+  render() {
+    const itemsBG = this.createItems(this.props.sItemsByGenre);
+
+    return (
+      <div
+        className="uk-text-center uk-child-width-1-3 uk-grid-match"
+        uk-grid="true"
+      >
+        {itemsBG}
+      </div>
+    );
+  }
 }
