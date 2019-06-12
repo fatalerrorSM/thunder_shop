ac1cf45b399f27662bbf9431447b8c7e284256e8
Fatal Error <fatalerror.sm@gmail.com>
2019-06-12 14:49:21 +0300

    creating a cart-item component for main cart component

--- client/src/components/cart-item/cart-item.tsx
+++ client/src/components/cart-item/cart-item.tsx
@@ -1,21 +1,42 @@
 import * as React from "react";
 
-interface ICartItem extends React.Props<any>{
-    _id : string;
-    name: string;
+import "./cart-item.css";
+
+interface ICartItem extends React.Props<any> {
+  _id: string;
+  name: string;
+  price: string;
+  discount: number;
+  onClickDeleteFromCart: any;
 }
 
 export default class CartItem extends React.Component<ICartItem> {
-    render(){
-        return(
-            <div className="cart-list">
-                <div className="cart-list-item-name">
-                    {this.props.name}
-                </div>
-                <div className="cart-list-item-price">
-                        
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const discount_price = this.props.discount ? (
+      <span className="price">({this.props.price})</span>
+    ) : null;
+    const price =
+      parseFloat(this.props.price) -
+      (parseFloat(this.props.price) * this.props.discount) / 100;
+
+    return (
+      <div className="uk-grid-small" uk-grid="true">
+        <div className="uk-width-expand" uk-leader="fill: .">
+          {this.props.name}
+        </div>
+        <div>
+          {price}$ {discount_price}
+        </div>
+        <hr className="divider uk-divider-vertical" />
+        <div>
+          <button
+            className="delete-button uk-button uk-button-default"
+            onClick={() => this.props.onClickDeleteFromCart(this.props._id)}
+          >
+            <span uk-icon="icon: minus-circle; ratio: 1" />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
